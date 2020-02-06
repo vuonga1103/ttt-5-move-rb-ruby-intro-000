6777d5f2d288dca1b3cc78cd966d13d50ce4398b
Anh Vuong <vuonga1103@yahoo.com>
2020-02-06 19:30:32 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,6 +9,7 @@ end
 # code your input_to_index and move method here!
 def input_to_index(user_input)
   converted_input = (user_input.to_i - 1)
+  return converted_input
 end
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
